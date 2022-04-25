e1aac352af473b179f78d4b48a82c2e1a687e5cb
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-04-25 21:22:23 +0300

    get use constructors

---
+++ user.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, name,username,password):
+        self.name = name
+        self.username = username
+        self.password = password
+        
+        user_accounts = []
+        
