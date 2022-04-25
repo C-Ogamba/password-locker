564a9a0558bc04ee0e5188df28a132fb2d2cc0ff
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-04-26 02:22:06 +0300

    displays all user accounts

--- user.py
+++ user.py
@@ -1,8 +1,30 @@
+from requests import delete
+
+
 class User:
-    def __init__(self, name,username,password):
-        self.name = name
-        self.username = username
+    def __init__(self,account,password):
+       
+        self.name = account
         self.password = password
         
         user_accounts = []
-        
+
+    def delete_account(self):
+        """function to delete account"""
+       
+       
+        User.user_accounts.remove(self)
+
+    def save_accounts(self):
+        """function to save account"""
+
+        User.user_accounts.append(self)
+
+    @classmethod
+    def display_account(cls):
+        """method to display all user accounts"""
+
+        return cls.user_accounts
+
+ 
+
