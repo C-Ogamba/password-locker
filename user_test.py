f7f40a9fc0af1a8535f5a5a45d02dd12a36b9a68
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-04-26 03:13:52 +0300

    test out instances created are appended to the grave

--- user_test.py
+++ user_test.py
@@ -10,4 +10,20 @@ class Test(unittest.TestCase):
 
         self.new_password = User("twitter", "123456")
 
-    
+    def test_init(self):
+        """test to check whether data entered will appear if called"""
+
+        self.assertEqual(self.new_password.account, "twitter")
+        self.assertEqual(self.new_password.password, "123456")
+
+    def test_save_account(self):
+        """checks whether value is appended to the user' password"""
+        self.new_password.save_account()
+        self.assertEqual(len(User.user_accounts), 1)
+
+    def test_save_multiple(self):
+
+        self.new_password.save_account()
+        test_pass = User("github", "654321")
+        test_pass.save_account()
+        self.assertEqual(len(User.user_passwords), 2)
