378461fd90029a707d40c93a030a25aa66dc4137
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-04-26 03:39:03 +0300

    testing whether saved password can be found

--- user_test.py
+++ user_test.py
@@ -18,23 +18,37 @@ class Test(unittest.TestCase):
 
     def test_save_account(self):
         """checks whether value is appended to the user' password"""
-        self.new_password.save_account()
+        self.new_password.save_accounts()
         self.assertEqual(len(User.user_accounts), 1)
 
     def test_save_multiple(self):
 
-        self.new_password.save_account()
+        self.new_password.save_accounts()
         test_pass = User("github", "654321")
-        test_pass.save_account()
+        test_pass.save_accounts()
         self.assertEqual(len(User.user_accounts), 2)
 
     def test_delete_account(self):
         """check whether the delete function removes accounts"""
 
-        self.new_password.save_account()
+        self.new_password.save_accounts()
         test_pass = User("github", "654321")
-        test_pass.save_account()
-        self.assertEqual(len(User.user_accounts), 2)
+        test_pass.save_accounts()
+        self.assertEqual(len(User.user_accounts), 1)
+
+    def test_display_account(self):
+        self.assertEqual(User.display_account(), User.user_accounts)
+
+    def test_find_account(self):
+        """test checks whether account password is saved"""
+        self.new_password.save_accounts()
+        test_pass = User("github", "654321")
+        test_pass.save_accounts()
+        found_account =User.find_by_account("github")
+        self.assertEqual(found_account.account, test_pass.account)
+
+    def account_exits(self):
+
 
 if __name__ == '__main__':
     unittest.main()   
